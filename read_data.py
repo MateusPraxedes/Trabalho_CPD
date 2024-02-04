7e19f8c623002a35b2240bb1b99fd36449091262
BernardoBoeira <150172305+BernardoBoeira@users.noreply.github.com>
2024-02-04 19:36:08 -0300

    mudança leitura arquivos

--- read_data.py
+++ read_data.py
@@ -3,6 +3,7 @@ import pickle
 from pathlib import Path
 import csv 
 import os
+from prettytable import PrettyTable
 
 caminho_arquivos = os.path.join(os.environ['USERPROFILE'],'Desktop')
 
@@ -10,53 +11,20 @@ caminho_arquivos = os.path.join(os.environ['USERPROFILE'],'Desktop')
 def le_dados():
     caminho_projeto = Path()    
     path_dados_bin =  Path(f"{caminho_projeto.absolute()}\\bin\\steam_data_bin.bin")
-        
-    if not path_dados_bin.is_file():
-        dados = []
-        with open(f"{caminho_arquivos}\\csv\\steam_data.csv",'r',encoding='utf-8') as arquivo:
-            reader = csv.reader(arquivo)
-            headers = next(reader)
-            print(headers)
-            '''
-                row[0] - url
-                row[1] - nome
-                row[2] - categoria
-                row[3] - url_img
-                row[4] - user reviews
-                row[5] - all_reviews
-                row[6] - data
-                row[7] - desenvolvedora
-                row[8] - distribuidora
-                row[9] - preco
-                row[10] - pegi
-                row[11] - pegi_url
-            '''
-            for row in reader:    
-                if row[1] != '-':
-                    dados.append(row)
-        
-        # Escreve os dados em um arquivo binário
-        with open(path_dados_bin, 'ab') as arquivo_binario:   # Abre o arquivo binário para escrita
-            for dado in dados:
-                arquivo_binario.write(bytes(str(dado),'utf-8'))
-                arquivo_binario.write('b\n')
-                
-        print("Dados gravados com sucesso no arquivo binário.")
-
-        
     
-    # Ler os dados do arquivo binário
-    with open(path_dados_bin, 'rb') as arquivo_binario:   # Abre o arquivo binário para leitura
-        dados_bin = arquivo_binario.read()
-        
-        dados = dados_bin.split(b'\n')
-        
-        print(dados_bin)
-        for dados in dados_bin:
-            print(dados.decode('utf-8'))
-            
-        jogos_lidos = len(dados_bin)
+    #Lê csv completo
+    df = pd.read_csv(f"{caminho_arquivos}\\csv\\steam_data.csv")
     
+    #Exclui colunas que nao seram tratadas
+    df = df.drop(["all_reviews","pegi","pegi_url"],axis=1)
    
-    return jogos_lidos
-    
+    # Exclui as linhas onde a coluna "name" é "-"
+    df = df.drop(df[df['name'] == '-'].index)
+
+    print(df.info())
+
+    with open(path_dados_bin,'wb') as arquivo_bin:
+        pickle.dump(df,arquivo_bin)
+        
+if  __name__ == "__main__":
+    le_dados()
